e308c9a5cfcab45376e50aec9db66f3cb3a00ef9
RetiPeti30 <peter.reti@stud.uni-corvinus.hu>
2022-09-27 22:30:00 +0200

    Az ötödik részfeladatig készen vannak a feladatok

---
+++ Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace Harmadikhet_DFP98U.Entities
+{ 
+public class User	
+{
+    public Guid ID { get; set; } = Guid.NewGuid();
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string Fullname
+    {
+        get
+        {
+            return string.Format(
+                "{0} {1}",
+                Lastname,
+                Firstname);
+        }
+    }
+}
+}
