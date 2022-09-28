dcc849ac80ade625fc949b0e7731096087028dd0
RetiPeti30 <peter.reti@stud.uni-corvinus.hu>
2022-09-28 23:15:45 +0200

    Entities rossz helyen?

--- Harmadikhet_DFP98U/Form1.cs
+++ Harmadikhet_DFP98U/Form1.cs
@@ -10,14 +10,20 @@ using System.Windows.Forms;
 
 namespace Harmadikhet_DFP98U
 {
+    BindingList<User> users = new BindingList<User>();
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName;
-            label2.Text = Resource1.FirstName;
-            button1.Text = Resource1.Add;
+            label1.Text = Resource2.LastName;
+            label2.Text = Resource2.FirstName;
+            button1.Text = Resource2.Add;
+
+            //listbox
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
 
         }
     }
